0757a5aa173bd5872b6d2d3b8576c17d0156287b
Gabrielly <gabriellysilva.oli01@gmail.com>
2024-08-07 16:37:32 -0300

    Adding event handling to the DOM

---
+++ script.js
@@ -0,0 +1,52 @@
+let textInput = document.querySelector('#text__area');
+let textOutput = document.querySelector('#message__content');
+
+function encrypt() {
+    let text = textInput.value.trim();
+    if (text === '') {
+        resetContent();
+        return;
+    }
+
+    let textEncrypted = text.replace(/e/g, 'enter')
+                            .replace(/i/g, 'imes')
+                            .replace(/a/g, 'ai')
+                            .replace(/o/g, 'ober')
+                            .replace(/u/g, 'ufat');
+
+    textOutput.innerHTML = '<textarea readonly id="text__output">' + textEncrypted + '</textarea>' +
+                           '<button class="buttons__copy" id="copy" onclick="copy()">Copy</button>';
+}
+
+function decrypt() {
+    let text = textInput.value.trim();
+    if (text === '') {
+        resetContent();
+        return;
+    }
+
+    let textDecrypted = text.replace(/enter/g, 'e')
+                            .replace(/imes/g, 'i')
+                            .replace(/ai/g, 'a')
+                            .replace(/ober/g, 'o')
+                            .replace(/ufat/g, 'u');
+
+    textOutput.innerHTML = '<textarea readonly id="text__output">' + textDecrypted + '</textarea>' +
+                           '<button class="buttons__copy" id="copy" onclick="copy()">Copy</button>';
+}
+
+function copy() {
+    let textCopy = document.getElementById('text__output');
+    textCopy.select();
+    document.execCommand('copy');
+
+    alert('Text copied!');
+}
+
+function resetContent() {
+    textOutput.innerHTML = '<img src="./assets/rabbit-daisy.png" alt="">' +
+                           '<h1>No messages found</h1>' +
+                           '<h3>Enter a text that you want to encrypt or decrypt.</h3>';
+}
+
+resetContent();
